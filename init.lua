0fe32a9ef4428fb9fd5cfd5a8c80e2ea1781ce9f
Paweł Piech <186290161+skipthedocs@users.noreply.github.com>
2024-12-30 13:52:05 +0100

    feat: initial commit

---
+++ init.lua
@@ -0,0 +1,5 @@
+-- This is a neovim configuration setup for daily coding work.
+-- It contains basic settings and plugins to make development more efficient.
+
+require("config.remap")
+require("config.lazy")
